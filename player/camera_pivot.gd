4a7935f6df690ceda15796097be891ceefeb1cb0
Devin Biddy <xp.gram@gmail.com>
2024-11-28 20:49:02 -0800

    add joystick camera look

--- player/camera_pivot.gd
+++ player/camera_pivot.gd
@@ -1,12 +1,14 @@
 extends Node3D
 
 @export_group("Camera")
-@export_range(0.0, 1.0) var mouse_sensitivity := 0.25
+@export_range(0.0, 1.0) var mouse_sensitivity := 0.15
+@export_range(0.0, 1,0) var stick_sensitivity := 0.25
 @export_range(-180, 180) var camera_upper_bound_degrees: int = -75
 @export_range(-180, 180) var camera_lower_bound_degrees: int = 30
 
 var CAMERA_UPPER_BOUND: float = camera_upper_bound_degrees / 180.0 * PI
 var CAMERA_LOWER_BOUND: float = camera_lower_bound_degrees / 180.0 * PI
+var CAMERA_STICK_SPEED: float = 12.0
 
 var _camera_mouse_input_direction := Vector2.ZERO
 
@@ -45,7 +47,21 @@ func _move_camera_by_mouse_motion(delta: float) -> void:
 
 func _move_camera_by_gamepad_stick(delta: float) -> void:
    ## Uses joystick input to move the camera.
-   pass
+   var raw_input := Input.get_vector(
+      "look_left",
+      "look_right",
+      "look_up",
+      "look_down",
+      0.4
+   )
+   # This step squares the "strength" of the input vector, allowing finer control near the
+   # lower end of the range.
+   var curved_input := raw_input * raw_input.length()
+   var scaled_input := curved_input * CAMERA_STICK_SPEED * stick_sensitivity * delta
+
+   rotation.y -= scaled_input.x
+   rotation.x -= scaled_input.y
+
 
 func _constrain_camera_angles_to_limits() -> void:
    ## Constrains the camera's pivot angle to predefined limits.
