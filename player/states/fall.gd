d3506c972072736b2290d004ee55888a1c51c453
Devin Biddy <xp.gram@gmail.com>
2025-08-09 18:55:20 -0700

    factor out duplicate code in fall and jump states

--- player/states/fall.gd
+++ player/states/fall.gd
@@ -6,12 +6,18 @@ extends State
 #   - Falling animation not applied until timer fully elapsed
 
 
+const PlayerMovement = preload("res://player/behaviors/movement.gd")
+
+
+# TODO Use preload() calls and such instead of nodes.
 @export_group('Transition-to States', 'state_')
 @export var state_idle: State
 @export var state_move: State
 
 
+# TODO What if this doesn't exist?
 @onready var _camera: Camera3D = %Camera3D
+
 
 var _last_movement_direction := Vector3.BACK
 
@@ -33,7 +39,18 @@ func process_physics(delta: float) -> State:
       0.4
    )
 
-   _handle_movement_input(delta, raw_input)
+   # TODO This is still fairly obnoxious. I need some better way of handling the
+   #   conditional beneath this call.
+   var movement_direction = PlayerMovement.apply_vector_input_to_character_body(
+      delta,
+      raw_input,
+      parent,
+      _camera,
+      physics_properties,
+   )
+   if movement_direction != Vector3.ZERO:
+      _last_movement_direction = movement_direction
+
    _rotate_character_body(delta)
    parent.move_and_slide()
 
@@ -44,45 +61,6 @@ func process_physics(delta: float) -> State:
          return state_move
 
    return null
-
-
-func _handle_movement_input(delta: float, raw_input: Vector2) -> void:
-   # TODO This was pulled from player_3d.gd and likely isn't ready to go.
-   # TODO Also... this is the same as ground movement, yes? I think we need layers.
-   #   Or, if not layers, common scripts that can be enabled/disabled at will.
-
-   # This step squares the "strength" of the input vector, allowing finer control near the
-   # lower end of the range.
-   var curved_input: Vector2 = raw_input * raw_input.length()
-
-   var forward_vector := _camera.global_basis.z
-   var rightward_vector := _camera.global_basis.x
-
-   var move_direction := (
-      forward_vector * curved_input.y +
-      rightward_vector * curved_input.x
-   )
-
-   # Normalize the camera-angled movement vector onto the ground plane.
-   move_direction = move_direction.normalized() * curved_input.length()
-
-   # Calculate new velocity for this frame.
-   var lateral_velocity := Vector3(parent.velocity.x, 0, parent.velocity.z)
-   var new_velocity = lateral_velocity.move_toward(move_direction * physics_properties.prop_move_speed, physics_properties.prop_move_acceleration * delta)
-
-   var user_input_is_none := is_zero_approx(move_direction.length())
-   var velocity_in_dime_stop_range: float = (new_velocity.length() < physics_properties.prop_move_stopping_speed)
-
-   if user_input_is_none and velocity_in_dime_stop_range:
-      new_velocity = Vector3.ZERO
-
-   # Move the character
-   parent.velocity.x = new_velocity.x
-   parent.velocity.z = new_velocity.z
-
-   # Store the last input direction
-   if move_direction.length() > 0:
-      _last_movement_direction = move_direction
 
 
 # TODO Accept an argument instead of depending on script-globals?
--- player/states/jump.gd
+++ player/states/jump.gd
@@ -1,5 +1,8 @@
 extends State
 
+const PlayerMovement = preload("res://player/behaviors/movement.gd")
+
+
 @export_group('Transition-to States', 'state_')
 @export var state_idle: State
 @export var state_move: State
@@ -37,7 +40,18 @@ func process_physics(delta: float) -> State:
       0.4
    )
 
-   _handle_movement_input(delta, raw_input)
+   # TODO This is still fairly obnoxious. I need some better way of handling the
+   #   conditional beneath this call.
+   var movement_direction = PlayerMovement.apply_vector_input_to_character_body(
+      delta,
+      raw_input,
+      parent,
+      _camera,
+      physics_properties,
+   )
+   if movement_direction != Vector3.ZERO:
+      _last_movement_direction = movement_direction
+
    _rotate_character_body(delta)
    parent.move_and_slide()
 	
@@ -53,45 +67,6 @@ func process_physics(delta: float) -> State:
       return state_fall
 	
    return null
-
-
-func _handle_movement_input(delta: float, raw_input: Vector2) -> void:
-   # TODO This was pulled from player_3d.gd and likely isn't ready to go.
-   # TODO Also... this is the same as ground movement, yes? I think we need layers.
-   #   Or, if not layers, common scripts that can be enabled/disabled at will.
-
-   # This step squares the "strength" of the input vector, allowing finer control near the
-   # lower end of the range.
-   var curved_input: Vector2 = raw_input * raw_input.length()
-   
-   var forward_vector := _camera.global_basis.z
-   var rightward_vector := _camera.global_basis.x
-   
-   var move_direction := (
-      forward_vector * curved_input.y +
-      rightward_vector * curved_input.x
-   )
-
-   # Normalize the camera-angled movement vector onto the ground plane.
-   move_direction = move_direction.normalized() * curved_input.length()
-
-   # Calculate new velocity for this frame.
-   var lateral_velocity := Vector3(parent.velocity.x, 0, parent.velocity.z)
-   var new_velocity = lateral_velocity.move_toward(move_direction * physics_properties.prop_move_speed, physics_properties.prop_move_acceleration * delta)
-
-   var user_input_is_none := is_zero_approx(move_direction.length())
-   var velocity_in_dime_stop_range: float = (new_velocity.length() < physics_properties.prop_move_stopping_speed)
-
-   if user_input_is_none and velocity_in_dime_stop_range:
-      new_velocity = Vector3.ZERO
-
-   # Move the character
-   parent.velocity.x = new_velocity.x
-   parent.velocity.z = new_velocity.z
-
-   # Store the last input direction
-   if move_direction.length() > 0:
-      _last_movement_direction = move_direction
 
 
 # TODO Accept an argument instead of depending on script-globals?
