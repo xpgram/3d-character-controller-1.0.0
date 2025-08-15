cf707f9b43528c1c166a6d655718dd1828df2670
Devin Biddy <xp.gram@gmail.com>
2025-08-14 22:06:02 -0700

    add right stick adjusts camera pivot (improperly, but it works)

--- player/scripts/camera/camera.gd
+++ player/scripts/camera/camera.gd
@@ -6,11 +6,16 @@ extends Node3D
                                    # TODO control_script can use bezier curves or whatever it wants.
 @export_subgroup('Camera Settings')
 @export var subject_displacement := Vector3(0, 1.0, 0)
+# TODO I should turn all these lerp rates into a type that auto-manages the values.
 ## How quickly the camera's position races to match the position of its subject.
 @export var position_lerp_rate_x: float = 24.0
 @export var position_lerp_rate_y: float = 6.0
 ## How quickly the camera's rotation races to face the position of its subject.
 @export var rotation_lerp_rate: float = 24.0
+## How quickly the camera's pivot rotation races to match its ideal orientation.
+@export var pivot_lerp_rate: float = 6.0
+@export var pivot_hor_max: float = 8.0
+@export var pivot_ver_max: float = 6.0
 
 # TODO I should pop these settings into a singleton node.
 @export_group('Control Settings')
@@ -51,7 +56,13 @@ func _physics_process(delta: float) -> void:
    # TODO Why 0.5? 180/360? This works, but why is it working?
    ideal_rotation.x = -Vector3.BACK.signed_angle_to(subject_yz_plane, Vector3.RIGHT) + 0.45
 
+    # Lerp camera to ideal rotation.
+   _camera.rotation.y = lerp(_camera.rotation.y, ideal_rotation.y, rotation_lerp_rate * delta)
+   _camera.rotation.x = lerp(_camera.rotation.x, ideal_rotation.x, rotation_lerp_rate * delta)
+
    # Allow stick input to affect camera.
+   # TODO This section is affected by the incorrect math of the camera point-to-target
+   #   section above. That's why rotations of the pivot look like rotations of the camera.
    var stick_vector = Input.get_vector(
       'look_left',
       'look_right',
@@ -59,10 +70,12 @@ func _physics_process(delta: float) -> void:
       'look_down',
       0.4
    )
-   # TODO It'd be nice if this had its own lerp lag.
-   ideal_rotation.y += -stick_vector.x * PI / 12.0
-   ideal_rotation.x += -stick_vector.y * PI / 16.0
-
-   # Lerp camera to ideal rotation.
-   _camera.rotation.y = lerp(_camera.rotation.y, ideal_rotation.y, rotation_lerp_rate * delta)
-   _camera.rotation.x = lerp(_camera.rotation.x, ideal_rotation.x, rotation_lerp_rate * delta)
+   var curved_stick_input: Vector2 = stick_vector * stick_vector.length()
+   var ideal_pivot_rotation := Vector3(
+      # TODO Magic numbers.
+      -curved_stick_input.y * PI * pivot_ver_max / 100.0,
+      -curved_stick_input.x * PI * pivot_hor_max / 100.0,
+      0,
+   )
+   _camera_pivot.rotation.y = lerp(_camera_pivot.rotation.y, ideal_pivot_rotation.y, pivot_lerp_rate * delta)
+   _camera_pivot.rotation.x = lerp(_camera_pivot.rotation.x, ideal_pivot_rotation.x, pivot_lerp_rate * delta)
