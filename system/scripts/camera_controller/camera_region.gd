5addcf043b63a4838b4849a91b9b72680ce58753
Devin Biddy <xp.gram@gmail.com>
2025-08-16 23:55:15 -0700

    add templates for what will be swappable camera controller scripts (for each room)

---
+++ system/scripts/camera_controller/camera_region.gd
@@ -0,0 +1,6 @@
+class_name CameraRegion
+extends Node
+
+# TODO How to add coins to score when colliding with coin, hm?
+#   I need to know how Player->Coin->Delete_Coin works in general before I can
+#   attach custom camera controllers to a colliding Player object.
