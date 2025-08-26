36cb41c773511a61447310d0965dc60b8079a285
Devin Biddy <xp.gram@gmail.com>
2025-08-25 18:11:30 -0700

    add doc strings to camera_jostler

--- player/camera/camera_jostler.gd
+++ player/camera/camera_jostler.gd
@@ -1,11 +1,14 @@
 class_name Jostler
 extends Node3D
 
-# TODO Use Jostler filters instead of different nodes.
+# TODO Use Jostler Strategies instead of different nodes.
 #   Jostler is adding unsteady-cam behavior, but what if I also want that walk-cycle-thing
 #   that all FPSes do? Wouldn't it be nice to have a list of resources or something that
 #   all return a position/rotation that just add together before being applied?
-# TODO This Jostler could be useful for things other than camera behavior. A washing machine, perhaps?
+# TODO Turn this script into a Resource.
+#   This Jostler could be useful for things other than camera behavior. A washing machine, perhaps?
+#   Or maybe we should just put the washing machine under a Jostler node? I'm honestly not
+#   sure how else it would work.
 
 const DEGREES_10 := PI / 18
 const DEGREES_5 := PI / 36
@@ -23,14 +26,21 @@ var global_active := true:
 
 ## The 'loudness' of all jostling movements. It is recommended to treat this
 ## normalized value as a volume knob for the entire system and describe
-## specific amplitude propertions in each component.
+## specific amplitude proportions in each component.
 @export_range(0, 1, 0.01, 'or_greater')
 var global_amplitude := 1.0
 
+## The rate at which the noise function is "traveled." Higher numbers yield faster jostling.
 @export_custom(PROPERTY_HINT_NONE, 'suffix:∝')
-var global_frequency := 20.0      ## The speed of the jostle.
+var global_frequency := 20.0
+
+## The width of the 1D noise texture used. Wider textures contain more unique noise.
+## # FIXME Looping widths are not implemented: the loop implied by this number is jerky.
 @export
-var global_noise_width := 100.0   ## The width of the 1D noise textures used. # FIXME This isn't really implemented.
+var global_noise_width := 100_000.0
+
+## The noise to sample. The actual noise sampled is duplicated from these settings and
+## given a random seed. A default exists if one is not provided.
 @export
 var global_noise := FastNoiseLite.new()
 
@@ -44,14 +54,25 @@ var rotation_active := true:
       if not on:
          _reset_rotation()
 
+# FIXME It bothers me that these are not real degrees. Do some testing.
+## The maximum range for rotations on each axis in degrees.
 @export_custom(PROPERTY_HINT_LINK, 'radians, suffix:°')
-var rotation_amplitude := Vector3(DEGREES_10, DEGREES_10, DEGREES_5) ## How large the maximum jostle movements are in degrees. # FIXME It bothers me that these are not real degrees. Do some testing.
+var rotation_amplitude := Vector3(DEGREES_10, DEGREES_10, DEGREES_5)
+
+## The rates for each axis at which noise is "traveled." Higher numbers yield faster jostling.
 @export_custom(PROPERTY_HINT_LINK, 'suffix:∝')
-var rotation_frequency := Vector3(1.0, 1.0, 1.0)   ## The amount of 'rumble'. Lower values yield slower movement. # TODO Actually, this isn't how Hertz work, is it?
+var rotation_frequency := Vector3(1.0, 1.0, 1.0)
+
+## The shift of the noise function on each axis as they relate to other jostle components
+## using the same noise.
 @export_custom(PROPERTY_HINT_LINK, 'suffix:s')
-var rotation_delay := Vector3(0.25, 0.25, 0.25)          ##
+var rotation_delay := Vector3(0.25, 0.25, 0.25)
+
+## The response curve for rotation noise values. A range over [0,1] describes an output
+## curve for both the positive and negative directions extending from point 0. Useful for
+## subduing overall motion while retaining the occasional spike.
 @export
-var rotation_steadiness_curve: Curve                  ## The response curve for the normalized values.
+var rotation_steadiness_curve: Curve
 
 @export_subgroup('Position Jostle', 'position_')
 
@@ -63,23 +84,36 @@ var position_active := true:
       if not on:
          _reset_position()
 
+## The maximum range for movement on each axis in meters.
 @export_custom(PROPERTY_HINT_LINK, 'suffix:m')
-var position_amplitude := Vector3(1.0, 1.0, 1.0)      ## How large the jostle movements are in degrees.
+var position_amplitude := Vector3(1.0, 1.0, 1.0)
+
+## The rates for each axis at which noise is "traveled." Higher numbers yield faster jostling.
 @export_custom(PROPERTY_HINT_LINK, 'suffix:∝')
-var position_frequency := Vector3(1.0, 1.0, 1.0)   ## The amount of 'rumble'. Lower values yield slower movement.
+var position_frequency := Vector3(1.0, 1.0, 1.0)
+
+## The shift of the noise function on each axis as they relate to other jostle components
+## using the same noise.
 @export_custom(PROPERTY_HINT_LINK, 'suffix:s')
-var position_delay := Vector3(0.0, 0.0, 0.0)          ##
+var position_delay := Vector3(0.0, 0.0, 0.0)
+
+## The response curve for position noise values. A range over [0,1] describes an output
+## curve for both the positive and negative directions extending from point 0. Useful for
+## subduing overall motion while retaining the occasional spike.
 @export
-var position_steadiness_curve: Curve                  ## The response curve for the normalized values.
+var position_steadiness_curve: Curve
 
 var x_noise: FastNoiseLite
 var y_noise: FastNoiseLite
 var z_noise: FastNoiseLite
 
+## The current coordinate position to sample from each noise function.
 var noise_cursor := 0.0
 
-var jostle_position := Vector3() # TODO Make private
-var jostle_rotation := Vector3()
+# TODO Add a tween on/off that adjusts global amplitude
+# func tween_amplitude(time_seconds: float, value: float)
+# func start(time_seconds: float, value: float = 1.0)
+# func stop(time_seconds: float, value: float = 0.0)
 
 
 func _ready() -> void:
@@ -132,6 +166,7 @@ func _reset_position() -> void:
    position = Vector3.ZERO
 
 
+## Physics process step for the rotation component.
 func _jostle_rotation() -> void:
    if not rotation_active:
       return
@@ -151,6 +186,7 @@ func _jostle_rotation() -> void:
    rotation.z = new_rotation_vector.z
 
 
+## Physics process step for the position component.
 func _jostle_position() -> void:
    if not position_active:
       return
@@ -166,6 +202,12 @@ func _jostle_position() -> void:
    position = new_position_vector
 
 
+## Returns a [Vector3] of noise values.
+## `param x_coordinate` is where along the noise function the values should be sampled.
+## `param amplitude_vector` controls the size of the values for each axis.
+## `param frequency_vector` controls relative density of the noise function's undulations.
+## `param delay_vector` controls the shift of each axis sample relative to other components.
+## `param steadiness_curve` maps the axis sample output to a bezier curve.
 func _get_vector_at_noise_coordinate(
    x_coordinate: float,
    amplitude_vector: Vector3,
@@ -195,5 +237,3 @@ func _get_vector_at_noise_coordinate(
    noise_vector = noise_vector * amplitude_vector * global_amplitude
 
    return noise_vector
-
-# TODO Add a tween on/off (adjusts global amplitude)
