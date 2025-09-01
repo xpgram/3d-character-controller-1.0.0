deaad9de7af703e44842b131e96f65dac09eade0
Devin Biddy <xp.gram@gmail.com>
2025-09-01 13:46:13 -0700

    silence unnecessary Event warnings

--- autoload/events.gd
+++ autoload/events.gd
@@ -1,5 +1,7 @@
 extends Node
 
+@warning_ignore_start('UNUSED_SIGNAL')
+
 signal camera_region_entered(entered_by: CameraTarget3D, region: CameraRegion3D)
 
 signal kill_plane_touched(body: PhysicsBody3D)
