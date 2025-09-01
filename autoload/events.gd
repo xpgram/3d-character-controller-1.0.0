0029f1a50493ba6c9131128b132d7d1e27deffc2
Devin Biddy <xp.gram@gmail.com>
2025-09-01 13:46:37 -0700

    add area_exited to CameraTarget3D signal emits

--- autoload/events.gd
+++ autoload/events.gd
@@ -3,6 +3,7 @@ extends Node
 @warning_ignore_start('UNUSED_SIGNAL')
 
 signal camera_region_entered(entered_by: CameraTarget3D, region: CameraRegion3D)
+signal camera_region_exited(exited_by: CameraTarget3D, region: CameraRegion3D)
 
 signal kill_plane_touched(body: PhysicsBody3D)
 signal flag_reached
--- nodes/camera/camera_target_3d.gd
+++ nodes/camera/camera_target_3d.gd
@@ -7,14 +7,22 @@ extends Area3D
 func _init() -> void:
    monitorable = false
    area_entered.connect(_on_area_entered)
+   area_exited.connect(_on_area_exited)
 
 
+## Handler for area_entered events.
 func _on_area_entered(area: Area3D) -> void:
    if area is not CameraRegion3D:
       return
    var camera_region := area as CameraRegion3D
 
-   # TODO How necessary is this if the Player3D is the one managing this?
-   #  This script could emit its own signal that Player3D listens for, which
-   #  then passes the controller on to its player camera.
    Events.camera_region_entered.emit(self, camera_region)
+
+
+## Handler for area_exited events.
+func _on_area_exited(area: Area3D) -> void:
+   if area is not CameraRegion3D:
+      return
+   var camera_region := area as CameraRegion3D
+
+   Events.camera_region_exited.emit(self, camera_region)
