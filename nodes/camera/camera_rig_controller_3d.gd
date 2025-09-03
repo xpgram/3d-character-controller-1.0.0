749a7070cbdc3fdf21dcdb4c0c76bf594b5de81b
Devin Biddy <xp.gram@gmail.com>
2025-09-02 18:18:35 -0700

    change rig_controller's interface to have one operate_rig() callable again

--- nodes/camera/camera_rig_controller_3d.gd
+++ nodes/camera/camera_rig_controller_3d.gd
@@ -5,30 +5,9 @@ extends Node3D
 
 ## The priority this controller script has over other controllers. Higher
 ## numbers take precedence over lower ones.
-@export_range(0, 9, 1)
+@export_range(0, 9, 1, 'or_greater')
 var controller_priority := 0
 
-## The subject of camera focus. Most scripts will orient their perspective
-## around this object, but this isn't guaranteed.
-@export
-var subject: Node3D
 
-
-func get_camera_position(_delta: float, _camera_rig: CameraRig3D) -> Vector3:
-   return Vector3.ZERO
-   # TODO Implement default camera_rig follow behavior.
-   # TODO Also, factor out a bunch of common camera_rig behaviors, like pointing at the subject.
-   # TODO Also, factor out the lerp-to-ideal behavior with customizable speed and such.
-
-
-func get_camera_pivot_rotation(_delta: float, _camera_rig: CameraRig3D) -> void:
-   pass
-
-
-func get_camera_arm_length(_delta: float, _camera_rig: CameraRig3D) -> float:
-   return 16.0
-
-
-func get_camera_settings(_delta: float, _camera_rig: CameraRig3D) -> void:
-   pass
-   # For controlling FOV and such.
+func operate_rig(_delta: float, _camera_rig: CameraRig3D) -> void:
+   return
