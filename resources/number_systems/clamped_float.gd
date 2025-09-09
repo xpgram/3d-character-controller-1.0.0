54aa7aa95cf228d209888b1ab34ba63827f7dffb
Devin Biddy <xp.gram@gmail.com>
2025-09-09 00:15:33 -0700

    add ClampedFloat resource

---
+++ resources/number_systems/clamped_float.gd
@@ -0,0 +1,108 @@
+@tool
+class_name ClampedFloat
+extends Resource
+
+# FIXME Infinite recursion where setters are concerned.
+
+
+## Emitted when the real number value changes.
+signal value_changed(value: float)
+
+## Emitted when the normalized value changes.
+signal value_normal_changed(value_normal: float)
+
+## Emitted when the minimum range value changes.
+signal min_value_changed(value: float)
+
+## Emitted when the maximum range value changes.
+signal max_value_changed(value: float)
+
+## Emitted when the real number value is equal to the minimum range.
+signal meter_empty()
+
+## Emitted when the real number value is equal to the maximum range.
+signal meter_full()
+
+
+## The real value of this clamped float.
+@export var value: float = 0:
+   set(new_value):
+      var old_value := value
+      value = clampf(new_value, min_value, max_value)
+      value_changed.emit(value)
+      _set_normalized_to_real_value()
+      _emit_value_targets(value, old_value)
+
+## The normalized value of this clamped float: a range between 0 and 1.
+@export var normalized_value: float = 0:
+   set(new_value):
+      var old_value := value
+      normalized_value = clampf(new_value, 0.0, 1.0)
+      value_normal_changed.emit(normalized_value)
+      _set_real_value_to_normalized()
+      _emit_value_targets(value, old_value)
+
+## The minimum real number value for this range.
+@export_range(0, 100, 0.1, 'or_greater; hide_slider') var min_value: float = 0:
+   set(new_value):
+      min_value = new_value
+
+      if min_value > max_value:
+         max_value = min_value
+      if min_value > value:
+         value = min_value
+
+      min_value_changed.emit(min_value)
+
+## The maximum real number value for this range.
+@export_range(0, 100, 0.1, 'or_greater; hide_slider') var max_value: float = 100:
+   set(new_value):
+      max_value = new_value
+
+      if max_value < min_value:
+         min_value = max_value
+      if max_value < value:
+         value = max_value
+
+      max_value_changed.emit(max_value)
+
+
+func _init() -> void:
+   value = min_value
+   max_value = max_value if max_value >= min_value else min_value
+
+   _set_normalized_to_real_value()
+
+
+## Emits signals when the value reaches set targets.
+func _emit_value_targets(new_value: float, old_value: float) -> void:
+   if new_value == min_value:
+      meter_empty.emit()
+   if new_value == max_value:
+      meter_full.emit()
+
+   # IMPLEMENT Other, export-config–set target points, like 30%, etc.
+   #  emission_points: Array[float] ## All normalized values
+   #  if old_value > emission_point and new_value < emission_point:
+   #     do()
+
+
+## Returns the size of the numeric range.
+func _get_range() -> float:
+   return max_value - min_value
+
+
+## Sets the normalized value to a number reflecting the current real value.
+func _set_normalized_to_real_value() -> void:
+   normalized_value = (value - min_value) / _get_range()
+
+
+## Sets the real value to a number reflecting the current normalized value.
+func _set_real_value_to_normalized() -> void:
+   value = (normalized_value * _get_range()) + min_value
+
+
+## Ensures that real and normalized values are all within the set range.
+func _clamp_values() -> void:
+   value = clampf(value, min_value, max_value)
+   _set_normalized_to_real_value()
