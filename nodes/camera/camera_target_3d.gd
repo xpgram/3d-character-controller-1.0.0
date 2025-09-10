8996142f465dec4c91335a2eefe9f25c9c7a76e6
Devin Biddy <xp.gram@gmail.com>
2025-09-09 17:03:53 -0700

    add todo

--- nodes/camera/camera_target_3d.gd
+++ nodes/camera/camera_target_3d.gd
@@ -3,6 +3,11 @@ class_name CameraTarget3D
 extends Area3D
 ## A type of [Area3D] for interacting with [CameraRegion3D]'s.
 
+# TODO Generalize CameraTarget3D
+#  Requiring that the player character have one Area3D for every interaction they might
+#  have is very silly. Implement these checks as a strategy that can be loaded into a
+#  general, point-like (small cube) collision object.
+
 
 func _init() -> void:
    monitorable = false
