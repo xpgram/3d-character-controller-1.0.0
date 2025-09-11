11768c463c19ac8cd5f954c6fb400c7f8e4c8fb7
Devin Biddy <xp.gram@gmail.com>
2025-09-10 22:34:52 -0700

    add a second set of transform values to CameraRigSettings that may be lerped to the transform described by export config

--- resources/camera/camera_rig_settings.gd
+++ resources/camera/camera_rig_settings.gd
@@ -1,13 +1,19 @@
 class_name CameraRigSettings3D
 extends Resource
+## A resource to contain the litany of [CameraRig3D] options. Also lightly manages some
+## of these options' values.
+
+# TODO Change this class' name.
+#  This kinda represents the Rig's transform, but I already have a struct-like object
+#  called CameraRigTransform, so... I dunno.
 
 
 @export_group('Transform')
 
-## If true, then the transform values set here will be instantly assumed by the camera
-## rig itself.
+## If true, then the transform values set here will be instantly assumed instead of being
+## tweened to.
 @export
-var skip_animation := false
+var disable_animation := false
 
 ## The position of the rig object.
 @export_custom(PROPERTY_HINT_NONE, 'suffix:m')
@@ -26,8 +32,8 @@ var pivot_rotation := Vector3.ZERO
 @export_custom(PROPERTY_HINT_NONE, 'suffix:m')
 var arm_length: float = 0
 
-## The point (using global coordinates) describing where the camera is looking. Also used
-## to inform the depth of field system.
+## A point in global space describing where the camera head is looking. Also used to
+## inform the depth of field system.
 @export_custom(PROPERTY_HINT_NONE, 'suffix:m')
 var focal_point := Vector3.ZERO
 
@@ -84,3 +90,66 @@ var pivot_arm_max_rotation := Vector3.ONE * PI
 ## How far the camera zooms in when the pivot arm is being rotated toward one of its axis
 ## limits. This means the actual zoom is proportional to the actual rotation.
 @export var camera_tilt_zoom_distance: float = 4.0
+
+
+## A transform that represents the Rig's actual position values. This is continuously
+## lerped toward the intended transform values described by the exported fields.
+var _actual_transform := CameraRigTransform3D.new()
+
+
+func _init() -> void:
+   skip_animation()
+
+
+## Returns transform values representing the intended transform of the Rig. That is, the
+## values that the Rig's actual transform is being animated toward.
+func get_intended_transform() -> CameraRigTransform3D:
+   var rig_transform := CameraRigTransform3D.new()
+   rig_transform.rig_position = rig_position
+   rig_transform.rig_rotation = rig_rotation
+   rig_transform.pivot_rotation = pivot_rotation
+   rig_transform.arm_length = arm_length
+   rig_transform.focal_point = focal_point
+   return rig_transform
+
+
+## Returns transform values representing the current transform of the Rig.
+func get_actual_transform() -> CameraRigTransform3D:
+   return _actual_transform
+
+
+## Moves the actual transform values toward the intended transform.
+func lerp_transform(delta: float) -> void:
+   if disable_animation:
+      skip_animation()
+      return
+   
+   # Apply vector displacements.
+   var complete_rig_position := rig_position + rig_position_displacement
+   var complete_focal_point := focal_point + focal_point_displacement
+
+   # Lerp actual_transform to the intended transform.
+   # TODO Is this level of granularity too much? Especially considering it probably suffers the 1.4 diagonal problem.
+   _actual_transform.rig_position.x = lerp(_actual_transform.rig_position.x, complete_rig_position.x, lerp_rate_rig_position.x * delta)
+   _actual_transform.rig_position.y = lerp(_actual_transform.rig_position.y, complete_rig_position.y, lerp_rate_rig_position.y * delta)
+   _actual_transform.rig_position.z = lerp(_actual_transform.rig_position.z, complete_rig_position.z, lerp_rate_rig_position.z * delta)
+
+   _actual_transform.rig_rotation.x = lerp_angle(_actual_transform.rig_rotation.x, rig_rotation.x, lerp_rate_rig_rotation.x * delta)
+   _actual_transform.rig_rotation.y = lerp_angle(_actual_transform.rig_rotation.y, rig_rotation.y, lerp_rate_rig_rotation.y * delta)
+   _actual_transform.rig_rotation.z = lerp_angle(_actual_transform.rig_rotation.z, rig_rotation.z, lerp_rate_rig_rotation.z * delta)
+
+   _actual_transform.pivot_rotation.x = lerp_angle(_actual_transform.pivot_rotation.x, pivot_rotation.x, lerp_rate_pivot_rotation.x * delta)
+   _actual_transform.pivot_rotation.y = lerp_angle(_actual_transform.pivot_rotation.y, pivot_rotation.y, lerp_rate_pivot_rotation.y * delta)
+   _actual_transform.pivot_rotation.z = lerp_angle(_actual_transform.pivot_rotation.z, pivot_rotation.z, lerp_rate_pivot_rotation.z * delta)
+
+   _actual_transform.arm_length = lerp(_actual_transform.arm_length, arm_length, lerp_rate_arm_length * delta)
+
+   _actual_transform.focal_point.x = lerp(_actual_transform.focal_point.x, complete_focal_point.x, lerp_rate_focal_point.x * delta)
+   _actual_transform.focal_point.y = lerp(_actual_transform.focal_point.y, complete_focal_point.y, lerp_rate_focal_point.y * delta)
+   _actual_transform.focal_point.z = lerp(_actual_transform.focal_point.z, complete_focal_point.z, lerp_rate_focal_point.z * delta)
+
+
+## Instantly applies the intended transform values to the actual transform, skipping all
+## lerp animations.
+func skip_animation() -> void:
+   _actual_transform = get_intended_transform()
