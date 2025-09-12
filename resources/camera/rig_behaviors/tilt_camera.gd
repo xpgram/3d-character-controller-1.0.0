05a762c361352396bba9471a071d50b406defe40
Devin Biddy <xp.gram@gmail.com>
2025-09-12 00:03:09 -0700

    fix tilt_camera export config to show degrees instead of radians

--- resources/camera/rig_behaviors/tilt_camera.gd
+++ resources/camera/rig_behaviors/tilt_camera.gd
@@ -1,24 +1,32 @@
+class_name CameraBehavior_TiltCamera
 extends ICameraRigBehavior3D
 ## A camera behavior to allow players to use their camera input axes to tilt the camera
 ## along the level track.
 
-# FIXME This script can't be loaded into the ICameraRigBehavior3D array as a resource.
-#  I'm actually not sure why. I guess I kinda know why? I may have to work around this by
-#  making these Nodes.
-
 
 const InputUtils = preload('uid://tl2nnbstems3')
 
-## How far in degrees the camera's pivot arm tilts. These are tilts over each axis, so x
-## actually represents vertical rotation.
-@export_custom(PROPERTY_HINT_LINK, 'radians, suffix:°')
-var max_tilt := Vector2.ONE * PI / 12
+## How far in degrees the camera's pivot arm tilts horizontally.
+@export_custom(PROPERTY_HINT_NONE, 'radians, suffix:°')
+var max_tilt_hor := PI / 12
 
-## How far the focal point moves away from the subject in the direction of the camera tilt.
+## How far in degrees the camera's pivot arm tilts vertically.
+@export_custom(PROPERTY_HINT_NONE, 'radians, suffix:°')
+var max_tilt_ver := PI / 12
+
+## How far horizontally the focal point moves away from the subject in the direction of
+## the camera tilt.
 @export_custom(PROPERTY_HINT_LINK, 'suffix:m')
-var max_look_ahead := Vector2(4.0, 2.0)
+var max_look_ahead_hor := 4.0
 
-## How far the camera zooms in while tilting.
+## How far vertically the focal point moves away from the subject in the direction of
+## the camera tilt.
+@export_custom(PROPERTY_HINT_LINK, 'suffix:m')
+var max_look_ahead_ver := 2.0
+
+## How far the camera zooms in when at max tilt. Zoom distance is proportional to input
+## vector length. The input vector is taken from the current input method's camera look
+## axes.
 @export_custom(PROPERTY_HINT_NONE, 'suffix:m')
 var tilt_zoom_distance := 4.0
 
@@ -31,8 +39,8 @@ func process(_delta: float, camera_rig: CameraRig3D) -> void:
 
    # Tilt the pivot arm.
    _rig_transform.pivot_rotation = Vector3(
-      -stick_input.y * max_tilt.x,
-      -stick_input.x * max_tilt.y,
+      -stick_input.y * max_tilt_hor,
+      -stick_input.x * max_tilt_ver,
       0.0,
    )
    # FIXME The lerp for this behavior is in camera_settings under pivot_rotation, and it
@@ -43,18 +51,14 @@ func process(_delta: float, camera_rig: CameraRig3D) -> void:
    # Retract arm while tilting.
    _rig_transform.arm_length = stick_input.length() * tilt_zoom_distance
 
-   # FIXME This focal point behavior cannot be additive with any other. It must override.
-   #  Actually, not true. I still think this raises an architectural issue. What if a
-   #  script can't just add a vector but needs to assume direct control over a property?
-   # FIXME This represents an architectural problem with what I'm attempting here.
    # FIXME Snap focal point to subject should probably be a seperate behavior.
    # Move the focal point to some distance between the subject and camera position.
    _rig_transform.focal_point = camera_rig.subject.position.lerp(camera_rig.position, stick_input.length())
 
    # Add extra tilt by moving the focal point laterally to the XY plane of the rig.
    var focal_point_displacement := Vector3(
-      -stick_input.x * max_look_ahead.x,
-      -stick_input.y * max_look_ahead.y,
+      -stick_input.x * max_look_ahead_hor,
+      -stick_input.y * max_look_ahead_ver,
       0.0,
    )
    _rig_transform.focal_point += focal_point_displacement.rotated(Vector3.UP, camera_rig.rotation.y)
