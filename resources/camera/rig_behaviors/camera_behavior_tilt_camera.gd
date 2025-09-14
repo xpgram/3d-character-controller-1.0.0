f258bf0abf73381b5cf1e3b0d82501a721df79e1
Devin Biddy <xp.gram@gmail.com>
2025-09-13 18:37:20 -0700

    add new config exports: enable_input, apply_additively, and max_arm_length

--- resources/camera/rig_behaviors/camera_behavior_tilt_camera.gd
+++ resources/camera/rig_behaviors/camera_behavior_tilt_camera.gd
@@ -11,6 +11,14 @@ const InputUtils = preload('uid://tl2nnbstems3')
 ## Whether smooth movement animations should be enabled.
 @export var enable_animation := true
 
+## Whether to accept user input. With no user input, a vector input of (0,0) is assumed.
+@export var enable_input := true
+
+## Whether this behavior should apply itself to a [CameraRig3D] additively, preserving
+## other behavior's output, or if false, that it should apply itself absolutely,
+## overwriting any previous output.
+@export var apply_additively := true
+
 ## How far in degrees the camera's pivot arm tilts horizontally.
 @export_custom(PROPERTY_HINT_NONE, 'radians, suffix:°')
 var max_tilt_hor := PI / 12
@@ -29,11 +37,15 @@ var max_look_ahead_hor := 4.0
 @export_custom(PROPERTY_HINT_LINK, 'suffix:m')
 var max_look_ahead_ver := 2.0
 
+## How far the camera normally sits when at minimum tilt.
+@export_custom(PROPERTY_HINT_NONE, 'suffix:m')
+var max_arm_length := 16.0
+
 ## How far the camera zooms in when at max tilt. Zoom distance is proportional to input
 ## vector length. The input vector is taken from the current input method's camera look
 ## axes.
 @export_custom(PROPERTY_HINT_NONE, 'suffix:m')
-var tilt_zoom_distance := 4.0
+var tilt_zoom_retraction_length := 4.0
 
 ## A proportion of delta time. Represents the speed at which this behavior lerps to its
 ## desired state.
@@ -52,7 +64,7 @@ func reset_behavior(_camera_rig: CameraRig3D) -> void:
 
 
 func update_camera_rig(delta: float, camera_rig: CameraRig3D) -> void:
-   target_stick_input = InputUtils.get_camera_look_vector()
+   target_stick_input = InputUtils.get_camera_look_vector() if enable_input else Vector2.ZERO
 
    if enable_animation:
       _lerp_values(delta)
@@ -76,13 +88,25 @@ func _lerp_values(delta: float) -> void:
 func _apply_state_to_rig(camera_rig: CameraRig3D) -> void:
    var mix_weight = clampf(actual_stick_input.length(), 0, 1)
 
-   camera_rig.pivot_rotation += Vector3(
+   # Determine additive component of new transforms.
+   var additive_pivot_rotation := camera_rig.pivot_rotation if apply_additively else Vector3.ZERO
+   var additive_arm_length := camera_rig.arm_length if apply_additively else 0.0
+
+   # Determine this behavior's component of new transforms.
+   var new_pivot_rotation := Vector3(
       -actual_stick_input.y * max_tilt_hor,
       -actual_stick_input.x * max_tilt_ver,
-      0.0
+      0.0,
    )
-   camera_rig.arm_length -= mix_weight * tilt_zoom_distance
-   camera_rig.focal_point = camera_rig.focal_point.lerp(camera_rig.position, mix_weight)
+   var new_arm_length := max_arm_length - (mix_weight * tilt_zoom_retraction_length)
+
+   # Apply new transforms.
+   camera_rig.pivot_rotation = additive_pivot_rotation + new_pivot_rotation
+   camera_rig.arm_length = additive_arm_length + new_arm_length
+   # Focal point lerping between actual position and rig position is not possible in
+   # absolute mode.
+   if not apply_additively:
+      camera_rig.focal_point = camera_rig.focal_point.lerp(camera_rig.position, mix_weight)
 
    # Add extra tilt by moving the focal point laterally to the XY plane of the rig.
    var focal_point_displacement := Vector3(
